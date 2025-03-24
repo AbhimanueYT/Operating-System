2ea97fe9451145acf3825ee37639d92a82344938
Abhimanue T D <103575176+AbhimanueYT@users.noreply.github.com>
2025-03-24 21:24:50 +0530

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+while true
+do
+echo "1)Add
+2)Sub
+3)Mul
+4)Div
+5)Exit"
+read x
+if [ $x -eq 5 ];then
+echo "Exiting..."
+break
+fi
+echo "Enter first number:"
+read a
+echo "Enter second number:"
+read b
+case $x in
+1) r=$(($a+$b))
+echo "Result: "$r
+;;
+2) r=$(($a-$b))
+echo "Result: "$r
+;;
+3) r=$(($a*$b))
+echo "Result: "$r
+;;
+4)
+if [ $b -eq 0 ];then
+echo "Not possible"
+else
+r=$(($a/$b))
+echo "Result: "$r
+fi
+;;
+*) echo "Invalid"
+;;
+esac
+done
